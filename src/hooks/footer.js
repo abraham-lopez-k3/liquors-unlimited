898b4c9bd03fd820b6632ab8b03ca53c2a3a8978
Abraham Lopez <abrahamlopezk4@gmail.com>
2024-09-04 12:01:05 -0500

    remove unneeded images

--- src/hooks/footer.js
+++ src/hooks/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import Map from './components/map';
-import GoogleIcon from '../img/download.png'; // Adjust the path according to your folder structure
+import GoogleIcon from '../img/google-icon.png'; // Adjust the path according to your folder structure
 
 
 const Footer = () => {
