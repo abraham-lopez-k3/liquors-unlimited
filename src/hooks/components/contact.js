d82b860b8ae68a23ac68859049f2342728cbb730
Abraham Lopez <abrahamlopezk4@gmail.com>
2024-09-04 14:46:01 -0500

    add header to contact page

--- src/hooks/components/contact.js
+++ src/hooks/components/contact.js
@@ -9,9 +9,9 @@ const Contact = () => {
                 <div className="home-wine-container">
                 
                     <div className="home-content">
-                    <h2>Get in Touch with Liquors Unlimited</h2>
+                    <h1>Get in Touch with Liquors Unlimited</h1>
                         <p>
-                            At Liquors Unlimited, we pride ourselves on offering an extensive selection of wines, spirits, and beers, along with exceptional customer service. Whether you're planning a special event, need advice on the perfect bottle, or just have a question, we're here to help. Feel free to reach out to us with any inquiries or feedback—your satisfaction is our top priority. Our knowledgeable team is ready to assist you with personalized recommendations and ensure you have a great shopping experience.
+                            At Liquors Unlimited, we pride ourselves on offering an extensive selection of wines and spirits, along with exceptional customer service. Whether you're planning a special event, need advice on the perfect bottle, or just have a question, we're here to help. Feel free to reach out to us with any inquiries or feedback—your satisfaction is our top priority. Our knowledgeable team is ready to assist you with personalized recommendations and ensure you have a great shopping experience.
                         </p>
 
                         <p>
