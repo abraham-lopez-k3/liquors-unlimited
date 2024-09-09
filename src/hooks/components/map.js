389cccb79d021ee1f77f429a7b74c68e4d6805a8
Abraham Lopez <abrahamlopezk4@gmail.com>
2024-09-09 18:59:03 -0500

    undue last commit

--- src/hooks/components/map.js
+++ src/hooks/components/map.js
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
+import { React, useState } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
 
 const containerStyle = {
   height: "300px",
@@ -11,50 +12,33 @@ const center = {
   lng: -88.548219,
 };
 
+
 const MyMapComponent = () => {
-  const mapContainerRef = useRef(null);
   const [isBigIntSupported] = useState(typeof BigInt !== 'undefined');
 
   if (!isBigIntSupported) {
     return <div>Error Loading Map</div>;  // Display a loading message
   }
 
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
-      version: "weekly",
-    });
 
-    let map;
+  const {isLoaded} = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+  });
 
-    loader.importLibrary('maps').then(() => {
-      if (typeof BigInt === 'undefined') {
-        // Handle BigInt unsupported scenario
-        console.error('BigInt is not supported in this environment.');
-        return;
-      }
 
-      map = new window.google.maps.Map(mapContainerRef.current, {
-        center: center,
-        zoom: 15,
-      });
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={15}
+    >
+      <Marker 
+        position={center} 
+      />
+    </GoogleMap> )
+    : <></>;
 
-      new window.google.maps.Marker({
-        position: center,
-        map: map,
-      });
-    }).catch(err => {
-      console.error('Error loading Google Maps API:', err);
-    });
-
-    return () => {
-      if (map) {
-        map = null; // Clean up map instance on unmount
-      }
-    };
-  }, []);
-
-  return <div ref={mapContainerRef} style={containerStyle} />;
 }
 
 export default MyMapComponent;
