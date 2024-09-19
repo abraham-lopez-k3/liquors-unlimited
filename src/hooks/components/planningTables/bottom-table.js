054381effa92ab804cc03eb56c45271d24f423cc
Abraham Lopez <abrahamlopezk4@gmail.com>
2024-09-19 13:34:17 -0500

    update planning table

--- src/hooks/components/planningTables/bottom-table.js
+++ src/hooks/components/planningTables/bottom-table.js
@@ -3,46 +3,58 @@ import { Table, FormGroup, FormControl } from 'react-bootstrap';
 
 const BottomTable = ({ handleChangeValue, drinksTotal, totalPercentage }) => {
     const [sizes, setSizes] = useState({
-        size: "0.75",
         drinksData: {
-            Beer: ['Beer', 20, ''],
-            Red: ['Red Wine', 12, 5],
-            White: ['White Wine', 28, 5],
-            Bourbon: ['Bourbon', 11, 17],
-            Scotch: ['Scotch', 7, 17],
-            Gin: ['Gin', 4, 17],
-            Vodka: ['Vodka', 16, 17],
-            Rum: ['Rum', 2, 17]
+            Beer: ['Beer', 20, '', 0],
+            Red: ['Red Wine', 12, 0.75, 5],
+            White: ['White Wine', 28, 0.75, 5],
+            Bourbon: ['Bourbon', 11, 0.75, 17],
+            Scotch: ['Scotch', 7, 0.75, 17],
+            Gin: ['Gin', 4, 0.75, 17],
+            Vodka: ['Vodka', 16, 0.75, 17],
+            Rum: ['Rum', 2, 0.75, 17]
         }
     });
 
-    const handleSizeChange = (event) => {
-        setSizes({
-            ...sizes,
-            size: event.target.value
-        });
+    const handleSizeChange = (drink, newSize) => {
+      
+        const newDrinksData = { ...sizes.drinksData };
+        newDrinksData[drink][2] = parseFloat(newSize);  // Update the size for each drink
+
+       
+        setSizes({ ...sizes, drinksData: newDrinksData });
     };
 
     const updatePercentage = (drink, value) => {
         const newDrinksData = { ...sizes.drinksData };
-        newDrinksData[drink] = [newDrinksData[drink][0], parseInt(value, 10) || 0, newDrinksData[drink][2]];
+        newDrinksData[drink] = [newDrinksData[drink][0], parseInt(value, 10) || 0, newDrinksData[drink][3]];
         setSizes({ ...sizes, drinksData: newDrinksData });
-        
+
         // Calculate total percentage and update it
         const updatedTotalPercentage = Object.values(newDrinksData).reduce((acc, item) => acc + item[1], 0);
         handleChangeValue('percentage', updatedTotalPercentage);
     };
 
     const renderPlanningTable = () => {
-        const data = ['Beer', 'Red', 'White', 'Bourbon', 'Scotch', 'Gin', 'Vodka', 'Rum'];
+        const data = Object.keys(sizes.drinksData);
 
         return data.map((val, i) => {
-            const [label, percentage, fifthSize] = sizes.drinksData[val];
-            const drinks = Math.round(100 * (percentage * drinksTotal / 100)) / 100;
-            const fifth = Math.round(10 * drinks / fifthSize) / 10;
-            const liter = Math.round(10 * fifth / 1.33) / 10;
-            const rowStyle = i % 2 === 0 ? 'even-row' : 'odd-row';
+            const [label, percentage] = sizes.drinksData[val];
+            const bottleSize = sizes.drinksData[val][2];
+
             
+            const totalDrinksForType = (percentage * drinksTotal) / 100;  // Total drinks for this type
+            const drinksPerBottle = val === 'White' || val === 'Red' ? 5 : 17;  // Assume 5 drinks for wine, 17 drinks for spirits
+            
+
+            const totalBottles = (totalDrinksForType / drinksPerBottle).toFixed(1);  // Total bottles required
+
+            console.log('totalDrinksForType ', totalDrinksForType);
+            console.log('drinksPerBottle ', drinksPerBottle);
+            console.log('bottleSize ', bottleSize);
+
+            console.log('totalBottles ', totalBottles);
+            const rowStyle = i % 2 === 0 ? 'even-row' : 'odd-row';
+
             return (
                 <tr key={i} className={rowStyle}>
                     <td>{label}</td>
@@ -59,13 +71,19 @@ const BottomTable = ({ handleChangeValue, drinksTotal, totalPercentage }) => {
                             />
                         </FormGroup>
                     </td>
-                    {sizes.size === '0.75' && fifthSize &&
-                        <td>{fifth}</td>
+                    <td>
+                        {bottleSize &&
+                            <select value={bottleSize} onChange={(event) => handleSizeChange(val, event.target.value)} className="bottle-size-select">
+                                <option value="0.75">0.75 Liter</option>
+                                <option value="1">1 Liter</option>
+                            </select>
+                        }
+                    </td>
+                   
+                    {val !== 'Beer' &&
+                        <td>{totalBottles}</td>
                     }
-                    {sizes.size === '1' && fifthSize &&
-                        <td>{liter}</td>
-                    }
-                    {!fifthSize &&
+                    {val === 'Beer' &&
                         <td></td>
                     }
                 </tr>
@@ -79,12 +97,8 @@ const BottomTable = ({ handleChangeValue, drinksTotal, totalPercentage }) => {
                 <tr>
                     <th>Type of Drink</th>
                     <th>Percentage Consumed <span className="total-percentage">({totalPercentage}%)</span></th>
-                    <th>
-                        <select value={sizes.size} onChange={handleSizeChange} className="liters-select">
-                            <option value="0.75">0.75 Liter</option>
-                            <option value="1">1 Liter</option>
-                        </select>
-                    </th>
+                    <th>Bottle Size</th>
+                    <th>Total in Bottles</th>
                 </tr>
             </thead>
             <tbody>
